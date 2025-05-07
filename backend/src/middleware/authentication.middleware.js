9852d341b4d9d9e2881a6b2e148a6195e4387539
mohamedbakeer2341 <mohamedbakeer2341@gmail.com>
2025-05-07 14:27:09 +0300

    adding authentication feature

---
+++ backend/src/middleware/authentication.middleware.js
@@ -0,0 +1,19 @@
+import { verifyToken } from "../utils/jwt.js";
+
+export const authenticate = async (req, res, next) => {
+  try {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+      throw new Error("No token provided");
+    }
+
+    const token = authHeader.split(" ")[1];
+    const decoded = verifyToken(token);
+
+    req.user = decoded;
+    next();
+  } catch (error) {
+    next({ status: 401, message: error.message });
+  }
+};
