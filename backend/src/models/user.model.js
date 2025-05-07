78237baefe64f1eac494f6fe64c6183f132fa750
mohamedbakeer2341 <mohamedbakeer2341@gmail.com>
2025-05-07 14:23:30 +0300

    adding user model

---
+++ backend/src/models/user.model.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    profilePicture: {
+      type: String,
+      default: "https://avatars.githubusercontent.com/u/152345399",
+    },
+
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
