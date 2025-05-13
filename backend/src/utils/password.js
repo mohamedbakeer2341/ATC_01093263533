b007e63ed89afd15ffd61dae53a8b24e1c2ce3de
mohamedbakeer2341 <mohamedbakeer2341@gmail.com>
2025-05-13 03:42:51 +0300

    Adding password.js tests

--- backend/src/utils/password.js
+++ backend/src/utils/password.js
@@ -1,7 +1,7 @@
 import bcrypt from "bcryptjs";
 
 export const hashPassword = async (password) => {
-  const salt = await bcrypt.genSalt(10);
+  const salt = await bcrypt.genSalt(Number(process.env.SALT_ROUNDS));
   return bcrypt.hash(password, salt);
 };
 
